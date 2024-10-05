a5f25de28956f6cb46a392071679b8f14fe1514f
UmeshOla <umeshola07@gmail.com>
2024-10-05 08:41:54 +0530

    change the preview page

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -4,32 +4,32 @@ import MarketplaceJSON from "../Marketplace.json";
 import axios from "axios";
 import { useState } from "react";
 import { GetIpfsUrlFromPinata } from "../utils";
-
+import { logo1 } from '../logo.png'
 export default function Marketplace() {
     const sampleData = [
         {
             "name": "NFT#1",
-            "description": "Alchemy's First NFT",
+            "description": "mewo's First NFT",
             "website": "http://axieinfinity.io",
-            "image": "https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+            "image": logo1,
             "price": "0.03ETH",
             "currentlySelling": "True",
             "address": "0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
         },
         {
             "name": "NFT#2",
-            "description": "Alchemy's Second NFT",
+            "description": "mewo's Second NFT",
             "website": "http://axieinfinity.io",
-            "image": "https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
+            "image": logo1,
             "price": "0.03ETH",
             "currentlySelling": "True",
             "address": "0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
         },
         {
             "name": "NFT#3",
-            "description": "Alchemy's Third NFT",
+            "description": "mewo's Third NFT",
             "website": "http://axieinfinity.io",
-            "image": "https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+            "image": logo1,
             "price": "0.03ETH",
             "currentlySelling": "True",
             "address": "0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
